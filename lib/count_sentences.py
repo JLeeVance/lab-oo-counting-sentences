64a8111cffb62d0fe9eab08e4474bc0f1c64f3b5
J Vance <justinleevance94@gmail.com>
2024-01-31 10:30:21 -0500

    fixed spacing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -5,18 +5,16 @@ class MyString:
     def __init__(self, value=''):
         self.value = value
 
-
     @property
     def value(self):
         return self._value
-    
+
     @value.setter
     def value(self, new_value):
         if type(new_value) != str:
             print("The value must be a string.")
         else:
             self._value = new_value
-    
 
     def is_sentence(self):
         if self.value.endswith("."):
@@ -40,17 +38,10 @@ class MyString:
         copy = self.value.replace("!" , "\n").replace("?" , "\n").replace("." , "\n").splitlines()
         length_of_sentences = [item for item in copy if len(item) >= 1]
         return len(length_of_sentences)
-            
-        
-
-
-
-
 
 string_1 = MyString("This is a sentence with a period. It has three sentences!! wow???")
 string_2 = MyString("This is a sentence without a period")
 string_3 = MyString("This is a sentence with a question mark?")
 
-# string_1.count_sentences()
 string_1.count_sentences()
 dir(str)
